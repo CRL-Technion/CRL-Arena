899ed97b3fd2e242f110cf39524a1e140af86629
Shaya Naimi <shaya@ucla.edu>
2021-08-10 15:54:23 +0300

    main_UDP is now for broadcasting UDP data to Ubuntu computer, while main_planner is for visualizing, running the planner, and sending plan data to the ubuntu computer. can be run at the same time if one is in pycharm and the other one is in terminal

--- main_UDP.py
+++ main_UDP.py
@@ -7,10 +7,7 @@ import sys
 import numpy
 import math
 
-from Grid import Grid
 import time
-
-CELL_SIZE = .3
 
 def euler_from_quaternion(quat):
     """
@@ -49,13 +46,6 @@ def euler_from_quaternion(quat):
 
 # Create listener
 listener = Listener(ListenerType.Local)
-if len(sys.argv) == 2:
-    grid = Grid(cell_size = float(sys.argv[1]))
-elif CELL_SIZE == None:
-    grid = Grid()
-else:
-    grid = Grid(cell_size = CELL_SIZE)
-grid.plot_init_heatmap()
 
 # start the listener
 listener.start()
@@ -91,23 +81,3 @@ while True:
             to_send.append(body_dict)
     print("what is being sent via UDP: ", to_send)
     server.update_data(json.dumps(to_send))
-    grid.reset_grid()
-    for ms in marker_sets:
-        name = ms.name
-        if name == "all":
-            continue
-        elif 'corner' in name.lower():
-            corners.append(ms.to_dict())
-            continue
-        body_type = -1 if "obst" in name.lower() else int(name[name.index('-')+1::])
-        markers = ms.positions
-        set_coords = []
-        for mi, marker_pos in enumerate(markers):
-            loc = [marker_pos.x, marker_pos.y]
-            set_coords.append(loc)
-            # print("location marker #{mi}: ", loc)
-        grid.add_body(body_type, set_coords, tolerance=0)
-    grid.process_corners(corners)
-    time.sleep(0.05)
-
-    grid.plot_render()
