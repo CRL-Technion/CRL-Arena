dea293c56ce1daa910142349aa87da8b555358d4
hadarkam <hadarkaminsky@gmail.com>
2021-09-04 10:39:45 +0300

    Update multi_agent_start_script.py

--- robot_setup/multi_agent_start_script.py
+++ robot_setup/multi_agent_start_script.py
@@ -1,7 +1,7 @@
 import os
 
-os.chdir("//")
-ip_addresses = open("../../../Users/crl/Downloads/ip_samples.txt", 'r')
+# os.chdir("//")
+ip_addresses = open("ip_samples.txt", 'r')
 for line in ip_addresses:
     os.system("gnome-terminal -- /bin/bash -c 'sshpass -v -p crl1234 ssh ubuntu@{ip}'".format(ip = line))
 ip_addresses.close()
